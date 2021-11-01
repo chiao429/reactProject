50095f1075cbd772c2edadd057772001b72cd8fc
chiao429 <blast30314@gmail.com>
2021-11-01 18:30:51 +0800

    '[modify]更改參數'

--- src/index.js
+++ src/index.js
@@ -5,35 +5,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
  
- /**
-  * React Componet
-  * 把程式碼以函式(或是class，後面會提到)包成模組，變成一個自己訂作的元素，然後用跟使用button、div這些元素一樣的語法在JSX中使用，也就是: 
-  * */
- function AppTest(){
-  return (<button>大家好</button>);
-}
-
-/**
- * 自製元素
- * 在其他地方可以使用，不用再重複寫一次
- * @returns 
- */
-function Progress(){
-  const barWidth="50%";
-  return(
-<div>
-  <div className="progress-back" style={{backgroudColor:"rgba(0,0,0,0,2)",width:"200px",height:"7px",borderRadius:"10px"}}> 
-  <div className="progress-bar" style={{backgroudColor:"#fe5196",width:barWidth,height:"100%",borderRadius:"10px"}}> </div>
-  </div>
-</div>
-
-
-  )
-}
 
 //呼叫一個物件=ReactDOM的render函式
 ReactDOM.render( //程式進入點
-  <div><progress></progress></div>,  //第一個參數:渲染到畫面上的內容 
+  <h1>Hello World!</h1>,  //第一個參數:渲染到畫面上的內容 
   document.getElementById('root') //第二個參數:React綁定的原始div元素
 );
 
